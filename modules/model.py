890146516a98afb1ae86c07bb54f472772eca77a
SebastianMerino <sebastian.merino@pucp.edu.pe>
2024-03-15 11:10:26 -0500

    training model with 3D convs

--- modules/model.py
+++ modules/model.py
@@ -305,6 +305,172 @@ class UNETv2(nn.Module):
             x = self.upAttention[idx](x)
 
         return self.final_block(x)
+    
+
+class UNETv3(nn.Module):
+    def __init__(
+        self,
+        in_channels=3,
+        out_channels=1,
+        features=[64, 128, 256, 512],
+        emb_dim=256,
+        residual=True,
+        attention_heads=4,
+        attention_res=[256, 512],
+        group_norm=True,
+    ):
+        super(UNETv3, self).__init__()
+
+        self.time_mlp = nn.Sequential(
+            PositionalEncoding(features[0]),
+            linear(features[0], emb_dim),
+            SiLU(),
+            linear(emb_dim, emb_dim),
+        )
+
+        # Initial convolutional layers
+        self.initial_block_x = Initial3DConvBlock(
+            emb_channels = emb_dim,
+            dropout = 0,
+            channels_3d = 8,
+            channels_freq = in_channels,
+            out_channels = features[0],
+            use_conv=False,
+            use_scale_shift_norm=True,
+            use_checkpoint=False,
+            residual = False,
+        )
+
+        self.initial_conv_y = conv_nd(2, out_channels, features[0], 1)
+        self.initial_block_y = ResBlock(
+            channels = features[0],
+            emb_channels = emb_dim,
+            dropout = 0,
+            out_channels = features[0],
+            use_conv = False,
+            use_scale_shift_norm = True,
+            dims = 2,
+            use_checkpoint = False,
+            residual=residual,
+            group_norm=group_norm
+        )
+
+        # Down part of UNET for X
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.downBlocksX = nn.ModuleList()
+        self.downAttentionX = nn.ModuleList()
+        for feature in features:
+            self.downBlocksX.append(
+                ResBlock(
+                    channels = feature,
+                    emb_channels = emb_dim,
+                    dropout = 0,
+                    out_channels = feature*2,
+                    use_conv = False,
+                    use_scale_shift_norm = True,
+                    dims = 2,
+                    use_checkpoint = False,
+                    residual=residual,
+                    group_norm=group_norm
+                ))
+            if feature in attention_res:
+                self.downAttentionX.append(AttentionBlock(
+                    feature*2, use_checkpoint=False, num_heads=attention_heads
+                ))
+            else:
+                self.downAttentionX.append(nn.Identity())
+
+        # Down part of UNET for Y
+        self.downBlocksY = nn.ModuleList()
+        self.downAttentionY = nn.ModuleList()
+        for feature in features:
+            self.downBlocksY.append(
+                ResBlock(
+                    channels=feature,
+                    emb_channels=emb_dim,
+                    dropout=0,
+                    out_channels=feature * 2,
+                    use_conv=False,
+                    use_scale_shift_norm=True,
+                    dims=2,
+                    use_checkpoint=False,
+                    residual=residual,
+                    group_norm=group_norm
+                ))
+            if feature in attention_res:
+                self.downAttentionY.append(AttentionBlock(
+                    feature * 2, use_checkpoint=False, num_heads=attention_heads
+                ))
+            else:
+                self.downAttentionY.append(nn.Identity())
+
+        # Up part of UNET
+        self.upBlocks = nn.ModuleList()
+        self.upConvs = nn.ModuleList()
+        self.upAttention = nn.ModuleList()
+        for feature in reversed(features):
+            self.upConvs.append(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2))
+            self.upBlocks.append(
+                ResBlock(
+                    channels = feature * 3,
+                    emb_channels = emb_dim,
+                    dropout = 0,
+                    out_channels = feature,
+                    use_conv = False,
+                    use_scale_shift_norm = True,
+                    dims = 2,
+                    use_checkpoint = False,
+                    residual=residual,
+                    group_norm=group_norm
+                ))
+            if feature in attention_res:
+                self.upAttention.append(AttentionBlock(
+                    feature, use_checkpoint=False, num_heads=attention_heads
+                ))
+            else:
+                self.upAttention.append(nn.Identity())
+
+
+        self.final_block = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x, y, t):
+        # x: IQ image
+        # y: Noisy Bmode
+        time_emb = self.time_mlp(t)
+
+        # Encoder for x
+        x = self.initial_block_x(x, time_emb)
+        skip_connectionsX = []
+        for idx in range(len(self.downBlocksX)):
+            skip_connectionsX.append(x)
+            x = self.pool(x)
+            x = self.downBlocksX[idx](x, time_emb)
+            x = self.downAttentionX[idx](x)
+
+        # Encoder for y
+        y = self.initial_conv_y(y)
+        y =  self.initial_block_y(y, time_emb)
+        skip_connectionsY = []
+        for idx in range(len(self.downBlocksY)):
+            skip_connectionsY.append(y)
+            y = self.pool(y)
+            y = self.downBlocksY[idx](y, time_emb)
+            y = self.downAttentionY[idx](y)
+        # Combination
+        x = x+y
+
+        # Convolutional layers and up-sampling
+        skip_connectionsX = skip_connectionsX[::-1]  # Reversing list
+        skip_connectionsY = skip_connectionsY[::-1]  # Reversing list
+        for idx in range(len(self.upBlocks)):
+            x = self.upConvs[idx](x)  # UpConvolution
+            concat_skip = torch.cat((skip_connectionsX[idx], x), dim=1)
+            concat_skip = torch.cat((skip_connectionsY[idx], concat_skip), dim=1)
+            x = self.upBlocks[idx](concat_skip, time_emb)  # Double convs
+            x = self.upAttention[idx](x)
+
+        return self.final_block(x)
 
 
 # PositionalEncoding Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
@@ -475,3 +641,147 @@ class QKVAttention(nn.Module):
         )  # More stable with f16 than dividing afterwards
         weight = th.softmax(weight.float(), dim=-1).type(weight.dtype)
         return th.einsum("bts,bcs->bct", weight, v)
+    
+
+# -------------------------------------- 3D things -------------------------------------- # 
+class ResBlock3D(nn.Module):
+    """ Residual block with 3D convolutions  """
+
+    def __init__(
+        self,
+        channels,
+        emb_channels,
+        dropout,
+        out_channels=None,
+        use_conv=False,
+        use_scale_shift_norm=False,
+        use_checkpoint=False,
+        residual = True,
+    ):
+        super().__init__()
+        self.channels = channels
+        self.emb_channels = emb_channels
+        self.dropout = dropout
+        self.out_channels = out_channels
+        self.use_conv = use_conv
+        self.residual = residual
+        self.use_checkpoint = use_checkpoint
+        self.use_scale_shift_norm = use_scale_shift_norm
+        dims = 3
+
+        self.in_layers = nn.Sequential(
+            nn.GroupNorm(4,channels),
+            SiLU(),
+            conv_nd(dims, channels, self.out_channels, 3, padding=1),
+        )
+        self.emb_layers = nn.Sequential(
+            SiLU(),
+            linear(
+                emb_channels,
+                2 * self.out_channels if use_scale_shift_norm else self.out_channels,
+            ),
+        )
+        self.out_layers = nn.Sequential(
+            nn.GroupNorm(4,self.out_channels),
+            SiLU(),
+            nn.Dropout(p=dropout),
+            zero_module(
+                conv_nd(dims, self.out_channels, self.out_channels, 3, padding=1)
+            ),
+        )
+
+        if residual:
+            if self.out_channels == channels:
+                self.skip_connection = nn.Identity()
+            elif use_conv:
+                self.skip_connection = conv_nd(
+                    dims, channels, self.out_channels, 3, padding=1
+                )
+            else:
+                self.skip_connection = conv_nd(dims, channels, self.out_channels, 1)
+        else:
+            self.skip_connection = None
+
+    def forward(self, x, emb):
+        """
+        Apply the block to a Tensor, conditioned on a timestep embedding.
+
+        :param x: an [N x C x ...] Tensor of features.
+        :param emb: an [N x emb_channels] Tensor of timestep embeddings.
+        :return: an [N x C x ...] Tensor of outputs.
+        """
+        return checkpoint(
+            self._forward, (x, emb), self.parameters(), self.use_checkpoint
+        )
+
+    def _forward(self, x, emb):
+        h = self.in_layers(x)
+        emb_out = self.emb_layers(emb).type(h.dtype)
+        while len(emb_out.shape) < len(h.shape):
+            emb_out = emb_out[..., None]
+        if self.use_scale_shift_norm:
+            out_norm, out_rest = self.out_layers[0], self.out_layers[1:]
+            scale, shift = th.chunk(emb_out, 2, dim=1)
+            h = out_norm(h) * (1 + scale) + shift
+            h = out_rest(h)
+        else:
+            h = h + emb_out
+            h = self.out_layers(h)
+        if self.residual:
+            h = self.skip_connection(x) + h
+        return h
+
+class Initial3DConvBlock(nn.Module):
+    def __init__(
+        self,
+        emb_channels,
+        dropout,
+        channels_3d,
+        channels_freq,
+        out_channels,
+        use_conv=False,
+        use_scale_shift_norm=False,
+        use_checkpoint=False,
+        residual = True,
+    ):
+        super().__init__()
+        self.emb_channels = emb_channels
+        self.dropout = dropout
+        self.out_channels = out_channels
+        self.use_conv = use_conv
+        self.residual = residual
+        self.use_checkpoint = use_checkpoint
+        self.use_scale_shift_norm = use_scale_shift_norm
+
+        self.initialConv = conv_nd(3, 1, channels_3d, kernel_size=3, padding=1)
+        self.resBlock1 = ResBlock3D(
+            channels = channels_3d,
+            emb_channels = emb_channels,
+            dropout = 0,
+            out_channels = channels_3d,
+            use_conv = False,
+            use_scale_shift_norm = True,
+            use_checkpoint = False,
+            residual=residual,
+        )
+        # self.resBlock2 = ResBlock3D(
+        #     channels = channels_3d,
+        #     emb_channels = emb_channels,
+        #     dropout = 0,
+        #     out_channels = channels_3d,
+        #     use_conv = False,
+        #     use_scale_shift_norm = True,
+        #     use_checkpoint = False,
+        #     residual=residual,
+        # )
+        self.outConv = conv_nd(2, channels_3d*channels_freq, out_channels, kernel_size=3, padding=1)        
+
+    def forward(self,x,emb):
+        x = x.unsqueeze(1)
+        x = self.initialConv(x)
+        x = self.resBlock1(x,emb)
+        # x = self.resBlock2(x,emb)
+        b,d,c,h,w = x.shape
+        x = torch.reshape(x,(b,d*c,h,w))
+        x =self.outConv(x)
+        return x
