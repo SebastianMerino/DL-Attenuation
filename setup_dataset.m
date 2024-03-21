42e056e52aca7e873932646d602a5f9a7ded4356
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2024-03-20 21:21:08 -0500

    correction

--- setup_dataset.m
+++ setup_dataset.m
@@ -91,8 +91,8 @@ windowing = windowing*ones(1,nx);
 % For looping
 refFiles = dir([refDir,'\*.mat']);
 Nref = length(refFiles);
-swrap = saran_wrap(band); % Correction factor for phantom data
-
+% swrap = saran_wrap(band); % Correction factor for phantom data
+swrap = 0;
 % Memory allocation
 Sp_ref = zeros(m,n,p,Nref);
 Sd_ref = zeros(m,n,p,Nref);
@@ -183,4 +183,38 @@ for iFile = 1:length(files)
     toc
 end
 
+end
+
+function [spect,psnr_Sp]=spectra(block,windowing,saran_layer,~,NFFT)
+% Computes the average of the parallel echoes spectra in the ROI
+%
+% Inputs:
+%   block           Data matrix, size nw x nx
+%   windowing       Window vector, size nw x nx (the same for all cols)
+%   saran_layer     Vector containing spectrum correction for the saran
+%                   layer, of size NFFT. 0 is case there is no correction.
+%   nw              Axial length of block (deprecated)
+%   NFFT            Number of FFT points
+%
+% Outputs:
+%   spect           Average of the parallel echoes spcetrin the ROI
+%   psnr_Sp         Log spectrum
+%       
+
+block = block - mean(block);
+block = block.*windowing;
+
+spect = abs(fft(block,NFFT,1));     % Fourier transform proximal window
+spect = spect.^2;                   % Sp is Intensity Now 
+
+spect = mean(spect,2);   % Sp is the averaga of the parallel echoes in the ROI
+
+
+% Saran-wrap correction factor for phantoms
+if all(saran_layer)
+    spect = spect./saran_layer;
+end
+
+% psnr_Sp = 10*log10(max(spect)./spect(end/2,:)); 
+psnr_Sp = mean(log(spect),2);
 end
