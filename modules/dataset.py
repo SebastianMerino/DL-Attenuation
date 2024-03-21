fb93bb07f4ff9d7e7ef370e8efe38636fdc2763f
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2024-03-21 16:57:21 -0500

    added new dataset from matlab

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
-import scipy as sp
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from pathlib import Path
+from scipy.signal import spectrogram
+from scipy.io import loadmat
 
 
 def get_sld(rf, fs, freq_L, freq_H, deltaF, blocksize, overlap_pc):
@@ -25,7 +26,7 @@ def get_spectra(rf, fs, freq_L, freq_H, deltaF, blocksize, overlap_pc):
     wz = int(blocksize*wl*(1-overlap_pc)/dz ); # Between windows
     nz = 2*int(blocksize*wl/dz /2 ); # Window size
 
-    f,t,spgram = sp.signal.spectrogram(rf, fs=fs, window=('tukey', 0.25), nperseg=nz, noverlap=nz-wz, nfft=nFFT, axis=0)
+    f,t,spgram = spectrogram(rf, fs=fs, window=('tukey', 0.25), nperseg=nz, noverlap=nz-wz, nfft=nFFT, axis=0)
     spgram.shape
     rang = np.squeeze((f > freq_L) & (f < freq_H))
     Slocal = spgram[rang,:,:]
@@ -66,33 +67,36 @@ class SpectrumDataset(Dataset):
 
         return x,y
     
-# class CustomDataset(Dataset):
-#     def __init__(self, data_folder):
-#         data_folder = Path(data_folder)
-#         self.input_folder = data_folder/'input'
-#         self.output_folder = data_folder/'output'
-#         self.data_file_list = sorted(os.listdir(self.input_folder))
+class CustomDataset(Dataset):
+    def __init__(self, data_folder, get_sld=True):
+        data_folder = Path(data_folder)
+        self.input_folder = data_folder/'input'
+        self.output_folder = data_folder/'output'
+        self.data_file_list = sorted(os.listdir(self.input_folder))
 
-#     def __len__(self):
-#         return len(self.data_file_list)
+    def __len__(self):
+        return len(self.data_file_list)
 
-#     def __getitem__(self, idx):
-#         input_path = os.path.join(self.input_folder, self.data_file_list[idx])
-#         output_path = os.path.join(self.output_folder, self.data_file_list[idx])
-#         sld = np.load(input_path)
-#         att_ideal = np.expand_dims(np.load(output_path),axis=0)
+    def __getitem__(self, idx):
+        input_path = os.path.join(self.input_folder, self.data_file_list[idx])
+        output_path = os.path.join(self.output_folder, self.data_file_list[idx])
+        
+        if get_sld:
+            x = loadmat(input_path)['sld']
+        else:
+            x = loadmat(input_path)['spectrum']
+            
+        y = np.expand_dims(loadmat(output_path)['acs'],axis=0)
+        
+        # input_transforms = transforms.Compose([
+            # torch.Tensor,
+            # transforms.Normalize(0,1.5)
+        # ])
+        # output_transforms = transforms.Compose([
+            # torch.Tensor,
+            # transforms.Normalize(1,1)
+        # ])
+        # x = input_transforms(x)
+        # y = output_transforms(y)
 
-#         input_transforms = transforms.Compose([
-#             torch.Tensor,
-#             transforms.Normalize(0,1.5)
-#         ])
-
-#         output_transforms = transforms.Compose([
-#             torch.Tensor,
-#             transforms.Normalize(1,1)
-#         ])
-
-#         x = input_transforms(sld)
-#         y = output_transforms(att_ideal)
-
-#         return x,y
+        return x,y
